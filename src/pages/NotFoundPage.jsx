e4ac88799873515e5576e78aad9c67bf2fff0142
Aleksis Daugats <98739151+JustMangoo@users.noreply.github.com>
2025-11-06 20:06:27 +0100

    Refactor NotFoundPage to include a styled 404 message and add WiltedPlant SVG asset

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,3 +1,11 @@
+import WiltedFlower from "../assets/WiltedPlant.svg";
+
 export default function NotFoundPage() {
-  return <div>404 - Page Not Found</div>;
+  return (
+    <>
+      <h1>404 </h1>
+      <h2>Page not found :&#40;</h2>
+      <img src={WiltedFlower} alt="Wilted plant" />
+    </>
+  );
 }
